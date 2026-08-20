b93f203661039b4f78bcabf9f83fc14f1f2b382b
Francois Gonzalez <francois.gnz@gmail.com>
2026-08-21 00:05:09 +0200

    #better parser

--- src/main.py
+++ src/main.py
@@ -1,52 +1,51 @@
-import sys
+import argparse
 
 DEFAULT_FUNCTIONS_FILE = "data/input/functions_definition.json"
 DEFAULT_PROMPTS_FILE = "data/input/function_calling_tests.json"
 DEFAULT_OUTPUT_FILE = "data/output/function_calls.json"
 
-file_config = {
-    "functions_definition": DEFAULT_FUNCTIONS_FILE,
-    "input": DEFAULT_PROMPTS_FILE,
-    "output": DEFAULT_OUTPUT_FILE
-}
 
-
-def load_input_files() -> None:
+def load_input_files(file_config: dict[str, str]) -> None:
     print(f"Functions definition file: {file_config['functions_definition']}")
     print(f"Prompt inputs file: {file_config['input']}")
     print(f"Output file: {file_config['output']}")
 
 
-def parse_args() -> None:
-    # Try to override the default config with provided args
-    if len(sys.argv) > 1:
-        try:
-            for i in range(1, len(sys.argv), 2):
-                argument = sys.argv[i]
-                argument_name = argument[2:]
+def parse_args(argv: list[str] | None = None) -> dict[str, str]:
+    parser = argparse.ArgumentParser(
+        prog="Call Me Maybe",
+        description="Translate prompts into functions calls",
+        usage=(
+            "[--functions_definition <function_definition_file>]"
+            "[--input <input_file>]"
+            "[--output <output_file>]"
+        )
+    )
 
-                if (
-                    argument.startswith("--", 0, 2)
-                    and argument_name in file_config.keys()
-                ):
-                    if (
-                        len(sys.argv) > i + 1
-                        and not sys.argv[i + 1].startswith("--", 0, 2)
-                    ):
-                        file_config[argument_name] = sys.argv[i + 1]
-                    else:
-                        raise Exception(f"Missing value for {argument}")
-                else:
-                    raise Exception(f"Argument '{argument}' is invalid")
+    parser.add_argument(
+        "--functions_definition",
+        default=DEFAULT_FUNCTIONS_FILE,
+        help="<path/to/functions_definition>"
+    )
 
-        except Exception as error:
-            print(error, file=sys.stderr)
-            sys.exit(1)
+    parser.add_argument(
+        "--input",
+        default=DEFAULT_PROMPTS_FILE,
+        help="<path/to/input_file>"
+    )
+
+    parser.add_argument(
+        "--output",
+        default=DEFAULT_OUTPUT_FILE,
+        help="<path/to/output_file>"
+    )
+
+    return vars(parser.parse_args(argv))
 
 
 def main() -> None:
-    parse_args()
-    load_input_files()
+    file_config = parse_args()
+    load_input_files(file_config)
 
 
 if __name__ == "__main__":
